09e5e4889b201075698b14d2a81ce825b4a85d21
Mechda <mechda.zariouh@gmail.com>
2023-07-29 00:47:14 +0200

    first commit

---
+++ src/main/java/com/example/tvshoptechiteasy/TvShopTechItEasyApplication.java
@@ -0,0 +1,13 @@
+package com.example.tvshoptechiteasy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TvShopTechItEasyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TvShopTechItEasyApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/tvshoptechiteasy/TvShopTechItEasyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.tvshoptechiteasy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TvShopTechItEasyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
